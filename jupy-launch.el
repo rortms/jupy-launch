571fd40178034a39af9fa99901f1f652cd4f6235
rortms <rortms@gmail.com>
2019-03-27 20:24:59 -0700

    Fix: Use comint to send commands to ans-term instance
    jupy-launch seemed to break when I switched to emacs 26. Sending commands
    to the running ansi-term instance through (comint-send-string) restores
    previous functionality. As I understand emacs shells are built with
    comint tools so this might be a more robust way. In addition
    ein:notebooklist-login seems to do the right thing and authenticates itself.
    No need for the ultra hacky ein-get-psswd-login function that would scrape
    the token from the terminal buffer.

--- jupy-launch.el
+++ jupy-launch.el
@@ -6,47 +6,32 @@
  jupyter notebook server in an ansi term buffer."
   
   (interactive)
-  (ansi-term "/bin/bash")
+  (let ( (curr-buff (current-buffer)) )
+    
+    (ansi-term "/bin/bash" "jupyter-server-buffer")
 
-  (insert (concat "cd " (read-directory-name "Enter work directory: ")))
-  (term-send-input)
-  (insert (concat "source activate " (read-string "Enter python environment: ")))
-  (term-send-input)
+    ;; Enter work directory
+    (comint-send-string "*jupyter-server-buffer*"
+                        (concat "cd " (read-directory-name "Enter work directory: ")))
+    (term-send-input)
 
-  (insert "jupyter notebook --no-browser")
-  (term-send-input)
-  (term-line-mode)
+    ;; Enter python environment
+    (comint-send-string "*jupyter-server-buffer*"
+                        (concat "source activate " (read-string "Enter python environment: ")))
+    (term-send-input)
+
+    ;; Launch jupyter server
+    (comint-send-string "*jupyter-server-buffer*"
+                        "jupyter notebook --no-browser")
+    (term-send-input)
+
+    (switch-to-buffer curr-buff)
+
+    )
+
   )
+
 (provide 'jupy-launch)
 
-(defun ein-get-psswd-and-login ()
-  "This function automates killing and yanking the jupyter server password from an ansi terminal. 
-Hacky and intended to be used only after calling jupy-launch: Notebook login will occur in default port."
-  (interactive)
-  (let ( (jup-buffer (get-buffer "*ansi-term*")))
-    (if jup-buffer
-        (progn
-          (switch-to-buffer "*ansi-term*")
-          (term-char-mode)
-          (beginning-of-buffer)
-          (search-forward "=")
-          (kill-line)
-          (end-of-buffer)          
-          (switch-to-buffer "*scratch*")
-          (ein:notebooklist-login 8888 (current-kill 0))
-          (ein:notebooklist-open 8888)
-          
-          ;; disable "next-input-history" command, is deleting cells          
-          (define-key ein:notebook-mode-map (kbd "M-n") nil)
-          
-          )))
-  )
-
-(provide 'ein-get-psswd-and-login)
-
-
-;; (async-shell-command (concat
-;;                       (concat "cd " (read-directory-name "Enter work directory: ")) "; "
-;;                       (concat "source activate " (read-string "Enter python environment: ")) "; "
-;;                       "jupyter notebook --no-browser") ";")
+;; (provide 'ein-get-psswd-and-login)
 
